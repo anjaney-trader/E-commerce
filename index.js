3161e67739ff308917b637431c606043e50b221d
anjaney-trader <130159729+anjaney-trader@users.noreply.github.com>
2024-03-20 22:16:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.post = ('/register', async (req, resp) => {
+app.post ('/register', async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
     result = result.toObject();
